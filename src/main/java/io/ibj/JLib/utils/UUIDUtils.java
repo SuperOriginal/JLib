c4bf8203a0c038dec4d58865bb97a5618d6cc1e5
Joe Hirschfeld <joe@ibj.io>
2014-10-20 18:11:11 -0400

    Added uuid utils

---
+++ src/main/java/io/ibj/JLib/utils/UUIDUtils.java
@@ -0,0 +1,24 @@
+package io.ibj.JLib.utils;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.util.UUID;
+
+/**
+ * Created by Joe on 10/20/2014.
+ */
+public class UUIDUtils {
+    public static byte[] toBytes(UUID id){
+        byte[] bytes = new byte[16];
+        ByteBuffer buffer = ByteBuffer.wrap(bytes);
+        buffer.order(ByteOrder.BIG_ENDIAN);
+        buffer.putLong(id.getMostSignificantBits());
+        buffer.putLong(id.getLeastSignificantBits());
+        return bytes;
+    }
+
+    public static UUID fromBytes(byte[] bytes){
+        ByteBuffer buffer = ByteBuffer.wrap(bytes);
+        return new UUID(buffer.getLong(),buffer.getLong());
+    }
+}
